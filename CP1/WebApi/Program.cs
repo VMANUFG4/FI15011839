a23e9168074e4d51b971f78274606693d2a30e1a
VMANUFG4 <vfuentes16794@ufide.ac.cr>
2025-10-21 20:41:56 -0600

    WebApi
    Funcionando

--- CP1/WebApi/Program.cs
+++ CP1/WebApi/Program.cs
@@ -1,3 +1,6 @@
+using System.Xml.Serialization;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,45 +16,82 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+//Uso de ChatGPT
 app.UseHttpsRedirection();
 
 var list = new List<object>();
 
 app.MapGet("/", () => Results.Redirect("/swagger"));
 
-app.MapPost("/", () => list);
+app.MapPost("/", (HttpContext context) =>
+{
+    bool xml = false;
+    if (context.Request.Headers.TryGetValue("xml", out var xmlHeader))
+    {
+        bool.TryParse(xmlHeader.ToString(), out xml);
+    }
+    if (xml)
+    {
+        var xmlSerializer = new XmlSerializer(typeof(List<object>));
+        using var stringWriter = new StringWriter();
+        xmlSerializer.Serialize(stringWriter, list);
+        return Results.Content(stringWriter.ToString(), "application/xml");
+    }
+    return Results.Ok(list);
+
+});
 
 app.MapPut("/", ([FromForm] int quantity, [FromForm] string type) =>
 {
+    if (quantity <= 0)
+    {
+        return Results.BadRequest(new { error = "La cantidad debe ser superior a cero" });
+    }
+    type = type.ToLower();
+    if (type != "int" && type != "float")
+    {
+        return Results.BadRequest(new { error = "El tipo debe ser 'int' o 'float'" });
+    }
     var random = new Random();
     if (type == "int")
     {
-        for (; quantity > 0; quantity--)
+        for (int i = 0; i < quantity; i++)
         {
             list.Add(random.Next());
         }
     }
-    else if (type == "float")
+    else 
     {
-        for (; quantity > 0; quantity--)
+        for (int i = 0; i < quantity; i++)
         {
             list.Add(random.NextSingle());
         }
     }
+    return Results.Ok(list);
 }).DisableAntiforgery();
 
 app.MapDelete("/", ([FromForm] int quantity) =>
 {
-    for (; quantity > 0; quantity--)
+    if (quantity <= 0)
+    {
+        return Results.BadRequest(new { error = "Por favor. La cantidad debe ser mayor a cero" });
+    }
+    if (list.Count < quantity)
+    {
+        return Results.BadRequest(new { error = $"La lista solo tiene {list.Count} elementos, no se pueden eliminar {quantity}" });
+    }
+    for (int i = 0; i < quantity; i++)
     {
         list.RemoveAt(0);
     }
+    return Results.Ok(list);
 }).DisableAntiforgery();
 
 app.MapPatch("/", () =>
 {
-    return Results.Ok();
+    list.Clear();
+    return Results.Ok(list);
 });
 
 app.Run();
+
